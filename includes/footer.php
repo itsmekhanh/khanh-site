1e67bd1f678d47f5d5610c747a6b0645d2cd39b5
Khanh Luc <khanh@classroom.tv>
2013-02-07 18:03:50 -0800

    updated footer

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="span4">
+                <p><b>Khanh Luc</b></p>
+                <p>San Jose, California</p>
+            </div>
+            <div class="span4">
+                <center>
+                    <p>If you decide to use my photos anywhere, please give me credit!</p>
+                    <p>&copy; 2012 - <?php echo date("Y");?> Khanh Luc. All Rights Reserved.</p>
+                </center>
+            </div>
+            <div class="span4">
+                <div class="contact">
+                    <p><b>Contact</b></p>
+                    <p>khanh.h.luc@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-31362963-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
