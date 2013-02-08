1c2c14cf7239b0d995dbf4f235185c957a87f9e8
Khanh Luc <khanh@classroom.tv>
2013-02-07 17:06:47 -0800

    updated

---
+++ events.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php 
+$page = "events";
+require_once './manager/phpFlickr.php';
+$flickr = new phpFlickr("63cee3b6bc72105b32cbed186ee52655");
+$photoset = $flickr->photosets_getList("49585808@N08");
+$sets=$photoset['photoset'];
+$count = 0;
+?>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <?php include_once './includes/header.php';?>
+        <?php include_once './includes/js.php'?>
+    </head>
+    <body>
+        <?php include_once './includes/navbar.php' ;?>
+        <div class="container" style="margin-top: 20px">
+            <?php if(isset($sets)): ?>
+                <?php for($i=count($sets)-1; $i>=0; $i--): ?>
+                    <?php if($count == 0): ?>
+                        <div class="row">
+                            <?php endif; ?>
+                            <div class="span2">
+                            <a href="./gallery?set=<?php echo $sets[$i]['id']?>" target="_blank">
+                                <img class="event-thumbnail border1 shadow1" src="<?php echo "http://farm{$sets[$i]['farm']}.static.flickr.com/{$sets[$i]['server']}/{$sets[$i]['primary']}_{$sets[$i]['secret']}_q.jpg"; ?>"/>
+                            </a>
+                            <p><center><?php echo $sets[$i]['title']; $count++; ?></center></p>
+                            </div>
+                        <?php if($count== 6): ?>
+                        </div>
+                        <?php $count = 0; ?>
+                        <?php endif; ?>
+                <?php endfor; ?>
+            <?php endif; ?>
+        </div>
+    </body>
+
+</html>
---
+++ galleries.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<?php 
+$page = "galleries"; 
+?>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <?php include_once './includes/header.php';?>
+        <?php include_once './includes/js.php'?>
+    </head>
+    <body>
+        <?php include_once './includes/navbar.php' ;?>
+        <div class="container" style="margin-top: 20px">
+            <div class="row">
+                <div class="span6" style="margin-bottom: 20px">
+                    <div class="border1 shadow1 main-gallery">
+                    <a id="people" href="./gallery?tags=people" target="_blank"><img src="./img/people.jpg"/></a>
+                    <center><h2>People</h2></center>
+                    </div>
+                </div>
+                <div class="span6" style="margin-bottom: 20px">
+                    <div class="border1 shadow1 main-gallery">
+                   <a id="animals" href="./gallery?tags=animals" target="_blank"><img src="./img/animals.jpg"/></a>
+                    <center><h2>Animals</h2></center>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span6" style="margin-bottom: 20px">
+                    <div class="border1 shadow1 main-gallery">
+                    <a id="travels" href="./gallery?tags=travels" target="_blank"><img src="./img/travels.jpg"/></a>
+                    <center><h2>Travels</h2></center>
+                    </div>
+                </div>
+                <div class="span6" style="margin-bottom: 20px">
+                    <div class="border1 shadow1 main-gallery">
+                    <a id="nature" href="./gallery?tags=nature" target="_blank"><img src="./img/nature.jpg"/></a>
+                    <center><h2>Nature</h2></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
